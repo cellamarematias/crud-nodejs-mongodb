916ea4293626c60365361204b9c0e9d86d30c66f
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-01-30 18:26:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
