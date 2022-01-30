8df38f16d339848fdc333d0039e8163aeb0828c1
cellamarematias <cellamarematias@gmail.com>
2022-01-30 18:21:13 -0300

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
