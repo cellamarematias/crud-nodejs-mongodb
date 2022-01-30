2cd7606bced083538a75065a55f62b7ecf7b5394
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-01-30 18:27:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 
 // parse application/x-www-form-urlencoded
@@ -35,7 +35,7 @@ app.use(express.static(__dirname + "/public"));
 
 //rutas WEB
 app.use('/', require('./router/rutas'));
-app.use('/mascotas', require('./router/mascotas'));
+app.use('/mascotas/', require('./router/mascotas'));
 
 
 app.use((req, res, next) => {
